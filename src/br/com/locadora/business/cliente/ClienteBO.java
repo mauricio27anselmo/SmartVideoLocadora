8c379dd6a6e7aa1aa8a56f3a9e20e851bbf28701
Mauricio Anselmo <m.anselmo93@gmail.com>
2018-05-04 17:56:37 -0300

    Inicio da migração da logica do Bean para o BO

--- src/br/com/locadora/business/cliente/ClienteBO.java
+++ src/br/com/locadora/business/cliente/ClienteBO.java
@@ -1,4 +1,135 @@
 package br.com.locadora.business.cliente;
 
+import br.com.locadora.business.enums.TipoTelefone;
+import br.com.locadora.business.telefone.Telefone;
+import br.com.locadora.business.telefone.TelefoneDAO;
+import br.com.locadora.util.FacesUtil;
+
+import java.util.List;
+
 public class ClienteBO {
+    private ClienteDAO dao;
+
+//    private Cliente novoCliente;
+//    private List<Cliente> listaClientes;
+//    private Telefone telResidencial;
+//    private Telefone telCelular;
+//    private Telefone telComercial;
+//
+//    public Cliente getNovoCliente() {
+//        if(novoCliente == null){
+//            novoCliente = new Cliente();
+//        }
+//        return novoCliente;
+//    }
+//
+//    public void setNovoCliente(Cliente novoCliente) {
+//        this.novoCliente = novoCliente;
+//    }
+//
+//    public List<Cliente> getListaClientes() {
+//        return listaClientes;
+//    }
+//
+//    public void setListaClientes(List<Cliente> listaClientes) {
+//        this.listaClientes = listaClientes;
+//    }
+//
+//    public Telefone getTelResidencial() {
+//        if(telResidencial == null){
+//            telResidencial = new Telefone();
+//            telResidencial.setNumero(null);
+//            telResidencial.setTipo(TipoTelefone.residencial);
+//        }
+//        return telResidencial;
+//    }
+//
+//    public void setTelResidencial(Telefone telResidencial) {
+//        this.telResidencial = telResidencial;
+//    }
+//
+//    public Telefone getTelCelular() {
+//        if(telCelular == null){
+//            telCelular = new Telefone();
+//            telCelular.setNumero(null);
+//            telCelular.setTipo(TipoTelefone.celular);
+//        }
+//        return telCelular;
+//    }
+//
+//    public void setTelCelular(Telefone telCelular) {
+//        this.telCelular = telCelular;
+//    }
+//
+//    public Telefone getTelComercial() {
+//        if(telComercial == null){
+//            telComercial = new Telefone();
+//            telComercial.setNumero(null);
+//            telComercial.setTipo(TipoTelefone.comercial);
+//        }
+//        return telComercial;
+//    }
+//
+//    public void setTelComercial(Telefone telComercial) {
+//        this.telComercial = telComercial;
+//    }
+//
+//
+//    public void salvar() {
+//        try{
+//            ClienteDAO dao = new ClienteDAO();
+//            TelefoneDAO dao2 = new TelefoneDAO();
+//            dao.incluir(novoCliente);
+//            novoCliente = dao.pesquisarPorNome(novoCliente.getNome());
+//            if(telComercial.getNumero() != null && telComercial.getNumero() != ""){
+//                telComercial.setIdCliente(novoCliente);
+//                dao2.incluir(telComercial);
+//            }
+//            if(telResidencial.getNumero() != null && telResidencial.getNumero() != ""){
+//                telResidencial.setIdCliente(novoCliente);
+//                dao2.incluir(telResidencial);
+//            }
+//            if(telCelular.getNumero() != null && telCelular.getNumero() != ""){
+//                telCelular.setIdCliente(novoCliente);
+//                dao2.incluir(telCelular);
+//            }
+//            FacesUtil.addMsgInfo("Cadastro realizado com sucesso!");
+//        }catch(RuntimeException ex){
+//            FacesUtil.addMsgErro("Erro no cadastro de cliente");
+//        }
+//    }
+//
+//    public void carregarLista(){
+//        try{
+//            ClienteDAO dao = new ClienteDAO();
+//            listaClientes = dao.listarTodos();
+//        }catch(RuntimeException ex){
+//            FacesUtil.addMsgErro("Erro na listagem de Clientes");
+//        }
+//    }
+
+    public ClienteBO(){
+        dao =  new ClienteDAO();
+    }
+
+    public void cadastrar(Cliente cliente){
+        try{
+            dao.incluir(cliente);
+            FacesUtil.addMsgInfo("Cadastro realizado com sucesso");
+        }catch(Exception ex){
+            ex.printStackTrace();
+            FacesUtil.addMsgErro("Erro ao cadastrar cliente");
+        }
+    }
+
+    public List<Cliente> listar(){
+        try{
+            return dao.listarTodos();
+        }catch(Exception ex){
+            ex.printStackTrace();
+            FacesUtil.addMsgErro("Erro ao listar clientes");
+            return null;
+        }
+
+    }
 }
--- src/br/com/locadora/controllers/beans/ClienteBean.java
+++ src/br/com/locadora/controllers/beans/ClienteBean.java
@@ -6,6 +6,7 @@ import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
 
 import br.com.locadora.business.cliente.Cliente;
+import br.com.locadora.business.cliente.ClienteBO;
 import br.com.locadora.business.cliente.ClienteDAO;
 import br.com.locadora.business.enums.TipoTelefone;
 import br.com.locadora.business.telefone.TelefoneDAO;
@@ -15,101 +16,25 @@ import br.com.locadora.util.FacesUtil;
 @ManagedBean
 @ViewScoped
 public class ClienteBean {
-	private Cliente novoCliente;
-	private List<Cliente> listaClientes;
-	private Telefone telResidencial;
-	private Telefone telCelular;
-	private Telefone telComercial;
-	
-	public Cliente getNovoCliente() {
-		if(novoCliente == null){
-			novoCliente = new Cliente();
-		}
-		return novoCliente;
-	}
 
-	public void setNovoCliente(Cliente novoCliente) {
-		this.novoCliente = novoCliente;
-	}
+    private Cliente novoCliente;
 
-	public List<Cliente> getListaClientes() {
-		return listaClientes;
-	}
-	
-	public void setListaClientes(List<Cliente> listaClientes) {
-		this.listaClientes = listaClientes;
-	}
-	
-	public Telefone getTelResidencial() {
-		if(telResidencial == null){
-			telResidencial = new Telefone();
-			telResidencial.setNumero(null);
-			telResidencial.setTipo(TipoTelefone.residencial);
-		}
-		return telResidencial;
-	}
+    private ClienteBO bo;
 
-	public void setTelResidencial(Telefone telResidencial) {
-		this.telResidencial = telResidencial;
-	}
+    public ClienteBean(){
+        this.novoCliente = new Cliente();
+        this.bo = new ClienteBO();
+    }
 
-	public Telefone getTelCelular() {
-		if(telCelular == null){
-			telCelular = new Telefone();
-			telCelular.setNumero(null);
-			telCelular.setTipo(TipoTelefone.celular);
-		}
-		return telCelular;
-	}
+    public Cliente getNovoCliente() {
+        return novoCliente;
+    }
 
-	public void setTelCelular(Telefone telCelular) {
-		this.telCelular = telCelular;
-	}
+    public void cadastrar(){
+         bo.cadastrar(novoCliente);
+    }
 
-	public Telefone getTelComercial() {
-		if(telComercial == null){
-			telComercial = new Telefone();
-			telComercial.setNumero(null);
-			telComercial.setTipo(TipoTelefone.comercial);
-		}
-		return telComercial;
-	}
-
-	public void setTelComercial(Telefone telComercial) {
-		this.telComercial = telComercial;
-	}
-
-	
-	public void salvar() {
-		try{
-			ClienteDAO dao = new ClienteDAO();
-			TelefoneDAO dao2 = new TelefoneDAO();
-			dao.incluir(novoCliente);
-			novoCliente = dao.pesquisarPorNome(novoCliente.getNome());
-			if(telComercial.getNumero() != null && telComercial.getNumero() != ""){
-				telComercial.setIdCliente(novoCliente);
-				dao2.incluir(telComercial);
-			}
-			if(telResidencial.getNumero() != null && telResidencial.getNumero() != ""){
-				telResidencial.setIdCliente(novoCliente);
-				dao2.incluir(telResidencial);
-			}
-			if(telCelular.getNumero() != null && telCelular.getNumero() != ""){
-				telCelular.setIdCliente(novoCliente);
-				dao2.incluir(telCelular);
-			}
-			FacesUtil.addMsgInfo("Cadastro realizado com sucesso!");
-		}catch(RuntimeException ex){
-			FacesUtil.addMsgErro("Erro no cadastro de cliente");
-		}	
-	}
-	
-	public void carregarLista(){
-		try{
-			ClienteDAO dao = new ClienteDAO();
-			listaClientes = dao.listarTodos();
-		}catch(RuntimeException ex){
-			FacesUtil.addMsgErro("Erro na listagem de Clientes");
-		}
-	}
+    public List<Cliente> listar(){
+        return bo.listar();
+    }
 }
