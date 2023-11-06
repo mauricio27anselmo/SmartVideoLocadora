5e069d497e9a57419bc8d3afd32ffcb2dcc184f7
Mauricio Matos <mauricio.santos@ana.gov.br>
2023-11-06 14:49:01 -0300

    Desenvolvimento inicial das telas de consulta, edição e listagem de clientes

--- src/main/java/br/com/locadora/bean/ClienteBean.java
+++ src/main/java/br/com/locadora/bean/ClienteBean.java
@@ -4,38 +4,45 @@ import br.com.locadora.datamodel.ClienteDataModel;
 import br.com.locadora.domain.Cliente;
 import br.com.locadora.service.ClienteService;
 import br.com.locadora.util.FacesUtil;
+import org.apache.commons.lang3.StringUtils;
 
 import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
-import javax.faces.bean.SessionScoped;
-import java.util.List;
+import javax.faces.bean.RequestScoped;
+import javax.faces.context.ExternalContext;
+import javax.faces.context.FacesContext;
+import java.util.Optional;
 
 @ManagedBean
-@SessionScoped
+@RequestScoped
 public class ClienteBean {
 
     private ClienteService clienteService;
 
-    private Cliente novoCliente;
-    private ClienteDataModel listaClientes;
+    private Cliente clienteForm;
 
-    public Cliente getNovoCliente() {
-        return novoCliente;
+    private ClienteDataModel clienteDataModel;
+
+    public Cliente getClienteForm() {
+        return clienteForm;
     }
 
-    public ClienteDataModel getListaClientes() {
-        return listaClientes;
+    public ClienteDataModel getClienteDataModel() {
+        return clienteDataModel;
     }
 
     @PostConstruct
     public void init() {
         clienteService = ClienteService.getInstance();
-        listar();
+        obterId();
+        if (!Optional.ofNullable(clienteForm).isPresent()) {
+            listar();
+        }
     }
 
     public void salvar() {
         try {
-            clienteService.save(novoCliente);
+            clienteService.save(clienteForm);
             FacesUtil.addMsgInfo("Cadastro realizado com sucesso!");
         } catch (Exception ex) {
             FacesUtil.addMsgErro("Erro no cadastro de cliente");
@@ -44,14 +51,35 @@ public class ClienteBean {
 
     private void listar() {
         try {
-            listaClientes = new ClienteDataModel(clienteService);
+            clienteDataModel = new ClienteDataModel(clienteService);
         } catch (Exception ex) {
             FacesUtil.addMsgErro("Erro ao listar cliente");
         }
     }
 
-    public String novo() {
-        novoCliente = new Cliente();
-        return "/pages/cliente/clienteManter.jsf?faces-redirect=true";
+    public void novo() {
+        clienteForm = new Cliente();
+        redirectToPage("/pages/cliente/clienteManter.xhtml");
+    }
+
+    private void obterId() {
+        try {
+            FacesContext facesContext = FacesContext.getCurrentInstance();
+            String id = facesContext.getExternalContext().getRequestParameterMap().get("id");
+            if (StringUtils.isNotBlank(id)) {
+                clienteForm = clienteService.findById(Long.parseLong(id));
+            }
+        } catch (Exception ex) {
+            FacesUtil.addMsgErro("Erro ao obter cliente");
+        }
+    }
+
+    private void redirectToPage(String pagePath) {
+        ExternalContext externalContext = FacesContext.getCurrentInstance().getExternalContext();
+        try {
+            externalContext.redirect(pagePath);
+        } catch (Exception e) {
+            FacesUtil.addMsgErro("Erro ao redirecionar pagina");
+        }
     }
 }
